1119ee2dbee9ab036224784237fdcdeceb0fcd13
Zaid <ztawalbeh8@gmail.com>
2024-10-09 21:19:01 +0300

    Create Credit Card Cycle

---
+++ Ketabi.Infra/Services/CreditCardService.cs
@@ -0,0 +1,44 @@
+﻿using Ketabi.Core.Data;
+using Ketabi.Core.Repositories;
+using Ketabi.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ketabi.Infra.Services
+{
+    public class CreditCardService : ICreditCardService
+    {
+        private readonly ICreditCardRepository _creditCardRepository;
+        public CreditCardService (ICreditCardRepository creditCardRepository)
+        {
+            _creditCardRepository = creditCardRepository;
+        }
+        public List<Creditcard> GetAllCredit()
+        {
+            return _creditCardRepository.GetAllCredit();
+        }
+        public void AddNewCredit(Creditcard creditcard)
+        {
+            _creditCardRepository.AddNewCredit(creditcard);
+        }
+        public void UpdateCredit(Creditcard creditcard)
+        {
+            _creditCardRepository.UpdateCredit(creditcard);
+        }
+        public void DeleteCredit(int id)
+        {
+            _creditCardRepository.DeleteCredit(id);
+        }
+        public Creditcard GetCreditById(int id)
+        {
+            return _creditCardRepository.GetCreditById(id);
+        }
+        public Creditcard GetCreditByUserId(int userId)
+        {
+            return _creditCardRepository.GetCreditByUserId(userId);
+        }
+    }
+}
---
+++ Ketabi/Controllers/CreditCardController.cs
@@ -0,0 +1,54 @@
+﻿using Ketabi.Core.Data;
+using Ketabi.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ketabi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditCardController : ControllerBase
+    {
+        private readonly ICreditCardService _creditCardService;
+        public CreditCardController (ICreditCardService creditCardService)
+        {
+            _creditCardService = creditCardService;
+        }
+        [HttpGet]
+        [Route("GetAllCredit")]
+        public List<Creditcard> GetAllCredit()
+        {
+            return _creditCardService.GetAllCredit();
+        }
+        [HttpPost]
+        [Route("AddNewCredit")]
+        public void AddNewCredit(Creditcard creditcard)
+        {
+            _creditCardService.AddNewCredit(creditcard);
+        }
+        [HttpPut]
+        [Route("UpdateCredit")]
+        public void UpdateCredit(Creditcard creditcard)
+        {
+            _creditCardService.UpdateCredit(creditcard);
+        }
+        [HttpDelete]
+        [Route("DeleteCredit")]
+        public void DeleteCredit(int id)
+        {
+            _creditCardService.DeleteCredit(id);
+        }
+        [HttpGet]
+        [Route("GetCreditById")]
+        public Creditcard GetCreditById(int id)
+        {
+            return _creditCardService.GetCreditById(id);
+        }
+        [HttpGet]
+        [Route("GetCreditByUserId")]
+        public Creditcard GetCreditByUserId(int userId)
+        {
+            return _creditCardService.GetCreditByUserId(userId);
+        }
+    }
+}
