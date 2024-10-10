15b6a5e29141008df6e4bdcba67e1e2bb6bb5ba0
Zaid <ztawalbeh8@gmail.com>
2024-10-10 12:18:58 +0300

    Cerate Borrowed Book Cycle

---
+++ Ketabi.Infra/Services/BorrowedBookService.cs
@@ -0,0 +1,49 @@
+﻿using Ketabi.Core.Data;
+using Ketabi.Core.DTO;
+using Ketabi.Core.Repositories;
+using Ketabi.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ketabi.Infra.Services
+{
+    public class BorrowedBookService : IBorrowedBookService
+    {
+        private readonly IBorrowedBookRepository _borrowedBookRepository;
+        public BorrowedBookService (IBorrowedBookRepository borrowedBookRepository)
+        {
+            _borrowedBookRepository = borrowedBookRepository;
+        }
+        public List<Borrowedbook> GetAllBorrowed()
+        {
+            return _borrowedBookRepository.GetAllBorrowed();
+        }
+        public void CreateNewBorrow(Borrowedbook borrowedbook)
+        {
+            _borrowedBookRepository.CreateNewBorrow(borrowedbook);
+        }
+        public void ChangeStatusToReturned(int id)
+        {
+            _borrowedBookRepository.ChangeStatusToReturned(id);
+        }
+        public void ChangeStatusToRequireFees(int id)
+        {
+            _borrowedBookRepository.ChangeStatusToRequireFees(id);
+        }
+        public void DeleteBorrowed(int id)
+        {
+            _borrowedBookRepository.DeleteBorrowed(id);
+        }
+        public Borrowedbook GetBorrowById(int id)
+        {
+            return _borrowedBookRepository.GetBorrowById(id);
+        }
+        public List<HistoryOfBorrowing> GethistoryOfborrowingByuserId(int userid)
+        {
+            return _borrowedBookRepository.GethistoryOfborrowingByuserId(userid);
+        }
+    }
+}
--- Ketabi/Program.cs
+++ Ketabi/Program.cs
@@ -15,15 +15,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Repositories
 builder.Services.AddScoped<IDbContext, DbContext>();
 builder.Services.AddScoped<ILibraryRepository, LibraryRepository>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 builder.Services.AddScoped<IBookReviewRepository, BookReviewRepository>();
 builder.Services.AddScoped<ICreditCardRepository, CreditCardRepository>();
+builder.Services.AddScoped<IBorrowedBookRepository, BorrowedBookRepository>();
+//Services
 builder.Services.AddScoped<ILibraryService, LibraryService>();
 builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddScoped<IBookReviewService, BookReviewService>();
 builder.Services.AddScoped<ICreditCardService, CreditCardService>();
+builder.Services.AddScoped<IBorrowedBookService, BorrowedBookService>();
 
 var app = builder.Build();
 
